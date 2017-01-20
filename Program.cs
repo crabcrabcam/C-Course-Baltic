e38b5ce1e185e30ab131a8df86210fb03b30b4c4
Cameron Reid <cam12win@gmail.com>
2017-01-20 15:24:29 +0000

    Done

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generate a new board of seats
+            Board board = new Board();
+
+            //The looping console app
+            while (true)
+            {
+                Console.WriteLine("The seat layout is:");
+                board.ShowSeats();
+                board.PickSeat();
+            }
+            
+        }
+    }
+}
